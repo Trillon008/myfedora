3fc6679f649b5d96123b87d20bd4f34a4e228618
Trillon008 <freego008@gmail.com>
2023-07-21 23:47:41 +0200

    remove flatpak delete fedora

--- scripts/pre/flatpakrepo.sh
+++ scripts/pre/flatpakrepo.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash 
 
 set -oue pipefail
+echo "Test"
 
-
-flatpak remote-delete fedora
-flatpak remote-add flathub --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+# flatpak remote-delete fedora
+# flatpak remote-add flathub --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
