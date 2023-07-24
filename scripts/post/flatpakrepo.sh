096ca28e3ce3ddab64ac374eaf13e9ae90bdf9a9
Trillon008 <freego008@gmail.com>
2023-07-24 14:32:02 +0200

    change flathub install

--- scripts/post/flatpakrepo.sh
+++ scripts/post/flatpakrepo.sh
@@ -2,11 +2,9 @@
 
 set -oue pipefail
 
-flatremote=$(flatpak remotes --columns=name)
-
-if [ ${flatremote} ] == 'fedora ']; then
-  flatpak remote-delete fedora
-  flatpak remote-add flathub --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
-fi
-
-unset ${flatremote}
+# flatremote=$(flatpak remotes --columns=name)
+# if [ ${flatremote} ] == 'fedora ']; then
+#   flatpak remote-delete fedora
+#   flatpak remote-add flathub --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+# fi
+# unset ${flatremote}
